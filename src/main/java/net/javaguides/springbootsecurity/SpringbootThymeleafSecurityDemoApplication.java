6877999a54509ef9e2c717ef6d94553108d4c333
salihmanisali <salihmanisali@gmail.com>
2019-12-19 16:47:08 +0300

    js css dosyalrı güncellendi

--- src/main/java/net/javaguides/springbootsecurity/SpringbootThymeleafSecurityDemoApplication.java
+++ src/main/java/net/javaguides/springbootsecurity/SpringbootThymeleafSecurityDemoApplication.java
@@ -1,7 +1,10 @@
 package net.javaguides.springbootsecurity;
 
+import org.h2.server.web.WebServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringbootThymeleafSecurityDemoApplication
@@ -11,4 +14,10 @@ public class SpringbootThymeleafSecurityDemoApplication
 		SpringApplication.run(SpringbootThymeleafSecurityDemoApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registration = new ServletRegistrationBean(new WebServlet());
+		registration.addUrlMappings("/console/*");
+		return registration;
+	}
 }
--- src/main/java/net/javaguides/springbootsecurity/web/FirmaController.java
+++ src/main/java/net/javaguides/springbootsecurity/web/FirmaController.java
@@ -8,9 +8,7 @@ import net.javaguides.springbootsecurity.repositories.OgrenciRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * @author Salih Efe
@@ -42,6 +40,16 @@ public class FirmaController {
 			model.addAttribute("firma", firma.get());
 		return "firma";
 	}
+
+	@GetMapping("/firma/delete/{id}")
+	public String deleteFirma(@PathVariable("id") Integer id, Model model) {
+		Firma firma = firmaRepository.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException("Invalid Firma Id:" + id));
+		firmaRepository.delete(firma);
+		model.addAttribute("firmalar", firmaRepository.findAll());
+		return "/firmalar";
+	}
+
 
 	@PostMapping("/firma")
 	public String saveFirma(Firma firma) {
