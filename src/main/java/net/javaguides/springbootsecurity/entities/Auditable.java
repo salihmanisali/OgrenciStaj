5e0789245b9a6764ded2800e7c9be3c64d2563d9
salihmanisali <salihmanisali@gmail.com>
2020-01-15 22:37:39 +0300

    audit ip

--- src/main/java/net/javaguides/springbootsecurity/entities/Auditable.java
+++ src/main/java/net/javaguides/springbootsecurity/entities/Auditable.java
@@ -1,39 +1,64 @@
 package net.javaguides.springbootsecurity.entities;
 
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import javax.persistence.*;
 import java.util.Date;
 
 
-@Getter(AccessLevel.PROTECTED)
-@Setter(AccessLevel.PROTECTED)
+@Data
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Auditable<U>
 {
     @CreatedBy
-    @Column(name = "created_by")
+    @Column(name = "created_by", updatable = false)
     private U createdBy;
 
     @CreatedDate
-    @Column(name = "created_date")
+    @Column(name = "created_date", updatable = false)
     private Date createdDate;
 
     @LastModifiedBy
-    @Column(name = "last_modified_by")
     private U lastModifiedBy;
 
     @LastModifiedDate
-    @Column(name = "last_modified_date")
     private Date lastModifiedDate;
+
+    private String createdByIp;
+    private String lastModifiedByIp;
+
+    @PrePersist
+    public void prePersist() {
+        createdByIp = getIPAuthenticatedUser();
+
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        lastModifiedByIp = getIPAuthenticatedUser();
+    }
+
+    private String getIPAuthenticatedUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || !authentication.isAuthenticated()) {
+            return null;
+        }
+        String ipAddress=null;
+        Object details = authentication.getDetails();
+        if (details instanceof WebAuthenticationDetails)
+            ipAddress = ((WebAuthenticationDetails) details).getRemoteAddress();
+
+        return ipAddress;
+
+    }
 }
